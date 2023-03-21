7f6ab96a22d64566a91f13b3f465cd9be1bd14fa
brayhan.zambrano <brayhan.zambrano@sofka.com.co>
2023-03-21 08:05:20 -0500

    add url for tests

--- src/main/java/com/magento/helpers/ToRemember.java
+++ src/main/java/com/magento/helpers/ToRemember.java
@@ -6,6 +6,9 @@ package com.magento.helpers;
 public enum ToRemember {
     URL_BASE,
     URL_REGISTER,
+    URL_WOMEN_TEES,
+    URL_MEN_TEES,
+    URL_GEAR_GAGS,
     USER_ACCOUNT,
     USER_EMAIL,
     USER_PASSWORD,
